7eaec3638025b8002538168c902fe33d51faa3e7
Tineo <tineo.dev@gmail.com>
2022-09-22 13:27:36 -0500

    Add pulgadas to cm

---
+++ pulgadas__cm.py
@@ -0,0 +1,8 @@
+
+print("CONVERTIDOR DE PULGADAS A CENTÍMETROS")
+
+pulgadas = int(input("Escriba una cantidad de pulgadas: "))
+
+pulgadas_cm = pulgadas * 2.54
+
+print(f"{pulgadas} pulgadas son {pulgadas_cm}cm")
