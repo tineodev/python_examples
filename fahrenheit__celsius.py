0a33dfcdee52c5b0bb28ab8ebef365a3d0dfbf9d
Tineo <tineo.dev@gmail.com>
2022-09-22 15:35:56 -0500

    Add Fahrenheit to Celsius

---
+++ fahrenheit__celsius.py
@@ -0,0 +1,8 @@
+
+print("CONVERTIDOR DE GRADOS FAHRENHEIT A GRADOS CELSIUS")
+
+fahrenheit = float(input("Escriba una temperatura en grados Fahrenheit: "))
+
+fahrenheit_C = (fahrenheit - 32) / 1.8
+
+print(f"{fahrenheit}ºF son {fahrenheit_C}ºC")
