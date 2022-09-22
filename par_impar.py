76f74034e3cd5717219c38492c7b6a219d3adf2f
Tineo <tineo.dev@gmail.com>
2022-09-22 18:11:41 -0500

    Add - par e impar - evalua los valores antes de pasar al siguiente

--- par_impar.py
+++ par_impar.py
@@ -1,11 +1,22 @@
 
 print("PAR E IMPAR (1)")
+
+
 numero1 = int(input("Escriba un número par: "))
+
+if numero1 % 2 != 0:
+  print("No ha escrito un número par.")
+  print("Ejecute de nuevo el programa para volver a intentarlo.")
+  exit()
+
+
 numero2 = int(input("Escriba un número impar: "))
 
-if numero1 % 2 == 0 and numero2 % 2 != 0:
-  print("¡Gracias por su colaboración!")
-else:
-  print("Uno o más de los valores que ha escrito no son correctos.")
+if numero2 % 2 == 0:
+  print("No ha escrito un número impar.")
   print("Ejecute de nuevo el programa para volver a intentarlo.")
+  exit()
+
+
+print("¡Gracias por su colaboración!")
 
