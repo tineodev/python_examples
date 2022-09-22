c43fce7c4a002448cc0b065af78fbf00f6ae2484
Tineo <tineo.dev@gmail.com>
2022-09-22 15:26:43 -0500

    Add pies to cm

---
+++ pies__cm.py
@@ -0,0 +1,8 @@
+
+print("CONVERTIDOR DE PIES A CENTÍMETROS")
+
+pies = int(input("Escriba una cantidad de pies: "))
+
+pies_cm = pies * 12 * 2.54
+
+print(f"{pies} pies son {pies_cm}cm")
--- pulgadas__cm.py
+++ pulgadas__cm.py
@@ -6,3 +6,4 @@ pulgadas = int(input("Escriba una cantidad de pulgadas: "))
 pulgadas_cm = pulgadas * 2.54
 
 print(f"{pulgadas} pulgadas son {pulgadas_cm}cm")
+
