c7abbd731e429bada48c2c4a7049dcb170132c62
Tineo <tineo.dev@gmail.com>
2022-09-22 17:57:04 -0500

    Add - par e impar - evalua valores al final

---
+++ par_impar.py
@@ -0,0 +1,11 @@
+
+print("PAR E IMPAR (1)")
+numero1 = int(input("Escriba un número par: "))
+numero2 = int(input("Escriba un número impar: "))
+
+if numero1 % 2 == 0 and numero2 % 2 != 0:
+  print("¡Gracias por su colaboración!")
+else:
+  print("Uno o más de los valores que ha escrito no son correctos.")
+  print("Ejecute de nuevo el programa para volver a intentarlo.")
+
