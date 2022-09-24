bc547849a8254f8ecb648137db30161eb7937d11
Tineo <tineo.dev@gmail.com>
2022-09-24 17:16:30 -0500

    Fix - detector de año bisiesto

--- bisiesto.py
+++ bisiesto.py
@@ -4,13 +4,13 @@ print("COMPROBADOR DE AÑOS BISIESTOS")
 year = int(input("Escriba un año y le diré si es bisiesto: "))
 
 if year % 4 == 0:
-  if year % 400 == 0 and year % 100 != 0:
-    print("El año es bisiesto")
   if year % 100 == 0:
-    print("El año no es bisiesto")
+    if year % 400 == 0:
+      print("El año es bisiesto porque es múltiplo de 4, 100 y de 400")
+    else:
+      print("El año no es bisiesto porque es múltiplo de 4, 100 y no de 400")
   else:
-    print("El año es bisiesto")
+    print("El año es bisiesto porque es múltiplo de 4")
 else:
-  print("El año no es bisiesto")
+  print("El año no es bisiesto porque no es múltiplo de 4")
 
-# ! Resolver bug
