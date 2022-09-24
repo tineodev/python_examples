675bd213515d8a4d096c58ea9d1e69f0b04f2a40
Tineo <tineo.dev@gmail.com>
2022-09-24 14:52:01 -0500

    Add - Comparador de años - print personalizado para diferencia de 1 año

--- comparador_years.py
+++ comparador_years.py
@@ -4,13 +4,19 @@ print("COMPARADOR DE AÑOS")
 año_actual = int(input("¿En qué año estamos? "))
 año_comparador = int(input("Escriba un año cualquiera: "))
 
-if año_comparador < año_actual:
-  diferencia = año_actual - año_comparador
+diferencia = año_actual - año_comparador
+
+if abs(diferencia) == 1:
+  if año_actual < año_comparador:
+    print(f"Para llegar al año {año_comparador} falta {abs(diferencia)} año.")
+  else:
+    print(f"Desde el año {año_comparador} ha pasado {diferencia} año.")
+
+elif diferencia == 0:
+  print("¡Son el mismo año!")
+
+elif año_actual < año_comparador:
+  print(f"Para llegar al año {año_comparador} falta {abs(diferencia)} años.")
+else:
   print(f"Desde el año {año_comparador} han pasado {diferencia} años.")
 
-elif año_comparador == año_actual:
-  print(f"¡Son el mismo año!")
-
-else: 
-  diferencia = año_comparador - año_actual
-  print(f"Para llegar al año {año_comparador} faltan {diferencia} años.")
