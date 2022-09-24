d82bf58623c2d1a0b884b41725c57a93a9eacc0b
Tineo <tineo.dev@gmail.com>
2022-09-24 15:44:14 -0500

    Add - multiplo de dos numeros - evita negativos y ceros

--- multiplo.py
+++ multiplo.py
@@ -3,6 +3,13 @@ print("COMPARADOR DE MÚLTIPLOS")
 numero_1 = int(input("Escriba un número: "))
 numero_2 = int(input("Escriba otro número: "))
 
+if numero_1 < 0 or numero_2 < 0:
+  print("Lo siento, este programa no admite valores negativos.")
+  exit()
+if numero_1 == 0 or numero_2 == 0:
+  print("Lo siento, este programa no admite valores nulos.")
+  exit()
+
 if numero_1 < numero_2:
   mayor = numero_2
   menor = numero_1
