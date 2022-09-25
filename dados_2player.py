a4d82d12bd8b965bb97ed0079b33e633cf54d162
Tineo <tineo.dev@gmail.com>
2022-09-25 16:52:53 -0500

    Add comments in file

--- dados_2player.py
+++ dados_2player.py
@@ -4,6 +4,7 @@ import random
 print("JUEGO DE DADOS - 2 JUGADORES")
 
 
+# * 2 jugadores
 player_1 = input("Jugador 01, introduzca su nombre: ")
 player_2 = input("Jugador 02, introduzca su nombre: ")
 
@@ -12,24 +13,33 @@ time.sleep(1)
 print("¡Qué comience el juego!")
 
 
+# * Dados player_1
 p1_dado1 = random.randint(1,6)
 p1_dado2 = random.randint(1,6)
 puntaje_p1 = p1_dado1 + p1_dado2
 time.sleep(1)
 print(f"{player_1} ha sacado un {p1_dado1} y un {p1_dado2}")
 
-dado1_p2 = random.randint(1,6)
-dado2_p2 = random.randint(1,6)
-puntaje_p2 = dado1_p2 + dado2_p2
+
+# * Dados player_2
+p2_dado1 = random.randint(1,6)
+p2_dado2 = random.randint(1,6)
+puntaje_p2 = p2_dado1 + p2_dado2
 time.sleep(1)
-print(f"{player_2} ha sacado un {dado1_p2} y un {dado2_p2}")
+print(f"{player_2} ha sacado un {p2_dado1} y un {p2_dado2}")
 
 
+# * Comparacion dados player_1
+
+
+# * Comparacion dados player_2
+
+
+# * Comparacion dados
 time.sleep(1)
-if dado_p1 < dado_p2:
-  print(f"Ha ganado {player_2}")
-elif dado_p1 > dado_p2:
+if puntaje_p1 > puntaje_p2:
   print(f"Ha ganado {player_1}")
+elif puntaje_p1 < puntaje_p2:
+  print(f"Ha ganado {player_2}")
 else:
-  print("Han empatado")
-
+  pass
