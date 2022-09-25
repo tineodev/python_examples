45111ed57beed833a244208c8fb67a478685e7d8
Tineo <tineo.dev@gmail.com>
2022-09-25 16:08:59 -0500

    Add program - Convertidor cm a km, m y cm - No imprime medidas innecesarias

--- cm__km_m_cm.py
+++ cm__km_m_cm.py
@@ -2,9 +2,10 @@
 import math
 
 print("CONVERTIDOR DE CM A KM, M Y CM")
-
 cantidad = int(input("Escriba una cantidad en centímetros: "))
 
+
+# * Comprueba que la cantidad ingresada no es cero, o negativo
 if cantidad == 0:
   print("Escriba una distancia mayor que cero.")
   exit()
@@ -15,6 +16,7 @@ else:
   pass
 
 
+# * Funcion que saca la cantidad de km
 if math.floor(cantidad/100000) != 0:
   cociente_km = math.floor(cantidad/100000)
   residuo_km = cantidad % 100000
@@ -23,6 +25,7 @@ else:
   residuo_km = cantidad
 
 
+# * Funcion que saca la cantidad de m
 if math.floor(residuo_km/100) != 0:
   cociente_m = math.floor(residuo_km/100)
   residuo_m = residuo_km % 100
@@ -31,10 +34,19 @@ else:
   residuo_m = residuo_km
 
 
+# * Funcion que saca la cantidad de cm
 residuo_cm = residuo_m
 
-print(f"{cantidad} centímetros son {cociente_km}km {cociente_m}m {residuo_cm}cm")
+
+# * Imprime los resultados
+print(f"{cantidad} centímetros son ", end="")
 
 
-
+# * No imprime valores innecesarios
+if cociente_km != 0:
+  print(f"{cociente_km}km ", end="")
+if cociente_m != 0:
+  print(f"{cociente_m}m ", end="")
+if residuo_cm !=0:
+  print(f"{residuo_cm}cm.")
 
