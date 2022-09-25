ca734b8b085472e943e4ebfdb9671ac5927f1a0b
Tineo <tineo.dev@gmail.com>
2022-09-25 16:34:15 -0500

    Add 2 jugadores & 2 dados & puntaje

--- dados_2player.py
+++ dados_2player.py
@@ -12,12 +12,17 @@ time.sleep(1)
 print("¡Qué comience el juego!")
 
 
-dado_p1 = random.randint(1,6)
+p1_dado1 = random.randint(1,6)
+p1_dado2 = random.randint(1,6)
+puntaje_p1 = p1_dado1 + p1_dado2
 time.sleep(1)
-print(f"{player_1} ha sacado un {dado_p1}")
-dado_p2 = random.randint(1,6)
+print(f"{player_1} ha sacado un {p1_dado1} y un {p1_dado2}")
+
+dado1_p2 = random.randint(1,6)
+dado2_p2 = random.randint(1,6)
+puntaje_p2 = dado1_p2 + dado2_p2
 time.sleep(1)
-print(f"{player_2} ha sacado un {dado_p2}")
+print(f"{player_2} ha sacado un {dado1_p2} y un {dado2_p2}")
 
 
 time.sleep(1)
