cdc9f2410f0e854f460bba536476ec7d7b5a3c24
Tineo <tineo.dev@gmail.com>
2022-09-26 07:50:53 -0500

    Add obtener_valor_1player's code

--- obtener_valor_1player.py
+++ obtener_valor_1player.py
@@ -1,3 +1,35 @@
 
+import random
+
+
 print("OBTENER VALOR")
+
+
+num_dados = int(input("Número de dados: "))
+if num_dados < 1:
+  print("¡Imposible!")
+  exit()
+
+num_valor = int(input("Valor a conseguir: "))
+if num_valor < 1 or num_valor > 7:
+  print(f"¡Imposible conseguir un {num_valor}!")
+  exit()
+
+
+desenlace = False
+
+
+print("Dados: ", end="")
+for i in range(num_dados):
+  dado_valor = random.randint(1,6)
+  print(f"{dado_valor} ", end="")
+  if dado_valor == num_valor:
+    desenlace = True
+print()
+
+
+if desenlace == True:
+  print("El jugador ha ganado")
+else:
+  print("El jugador ha perdido")
 
