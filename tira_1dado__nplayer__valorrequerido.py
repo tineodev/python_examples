88b6022ee04cd2f92e2f249974c6d64bd8eb9ee2
Tineo <tineo.dev@gmail.com>
2022-09-26 07:31:16 -0500

    Add tira_1dado_nplayer_valorrequerido's code

--- tira_1dado__nplayer__valorrequerido.py
+++ tira_1dado__nplayer__valorrequerido.py
@@ -1,5 +1,24 @@
 
+import random
 
 print("OBTENER VALOR")
 
+num_players = int(input("Número de jugadores: "))
+if num_players < 1:
+  print("¡Imposible!")
+  exit()
+
+num_valor = int(input("Valor a conseguir: "))
+if num_valor > 6 or num_valor < 1:
+  print(f"¡Imposible conseguir un {num_valor}!")
+  exit()
+
+
+for i in range(num_players):
+  valor_dado = random.randint(1,6)
+
+  if valor_dado == num_valor:
+    print(f"Jugador {i+1}: {valor_dado} CONSEGUIDO")
+  else:
+    print(f"Jugador {i+1}: {valor_dado}")
 
