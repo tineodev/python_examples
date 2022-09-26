69f7efea3a60ba54961b7f85f4e8ce9695324f56
Tineo <tineo.dev@gmail.com>
2022-09-26 07:14:00 -0500

    Add tira_1dado_nplayer's code

--- tira_1dado__nplayer.py
+++ tira_1dado__nplayer.py
@@ -1,4 +1,17 @@
 
+import random
+
+
 print("TIRADAS DE DADO")
 
+
+num_jugadores = int(input("Número de jugadores: "))
+if num_jugadores == 1:
+  print("¡Imposible!")
+  exit()
+
+
+for i in range(num_jugadores):
+  valor_dado = random.randint(1,6)
+  print(f"Jugador {i+1}: {valor_dado}")
 
