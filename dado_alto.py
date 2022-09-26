ea69e818b928d0ff786dfca79863fc8ddfc83f39
Tineo <tineo.dev@gmail.com>
2022-09-26 07:59:24 -0500

    Add code

--- dado_alto.py
+++ dado_alto.py
@@ -1,2 +1,29 @@
 
+import random
+
+
 print("EL DADO MÁS ALTO")
+
+
+num_dados = int(input("Número de dados: "))
+if num_dados < 1:
+  print("¡Imposible!")
+  exit()
+
+
+dado_mayor = 1
+
+
+
+print("Dados: ", end="")
+for i in range(num_dados):
+  dado_valor = random.randint (1, 6)
+  print(f"{dado_valor} ", end="")
+
+  if dado_mayor == dado_valor or dado_mayor < dado_valor:
+    dado_mayor = dado_valor
+print()
+
+
+print(f"El dado más alto es {dado_mayor}")
+
