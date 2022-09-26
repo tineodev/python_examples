dcc32b4c2404044eb43b1737db4f7d888811a1e6
Tineo <tineo.dev@gmail.com>
2022-09-26 07:08:20 -0500

    Add Tira_dados's code

--- tira_dados.py
+++ tira_dados.py
@@ -1,3 +1,18 @@
 
-print("TIRADA DE DADOS")
+import random
 
+
+print("TIRADA DE DADOS")
+num_tiradas = int(input("Número de tiradas:"))
+
+
+if num_tiradas == 0:
+  print("¡Imposible!")
+  exit()
+
+
+print("Dados: ", end="")
+for i in range(num_tiradas):
+  valor_dado = random.randint(1, 6)
+  print(f"{valor_dado} ", end="")
+print()
