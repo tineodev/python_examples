d1498c49a1ff0cd240c9e2a054d3a1c70ad0e16c
Tineo <tineo.dev@gmail.com>
2022-09-25 19:23:09 -0500

    Add creacion de listas a partir de negativo, positivo o cero

--- creacion_recta.py
+++ creacion_recta.py
@@ -1,3 +1,16 @@
 
-print("LSITAS DESDE CERO HASTA VALOR")
+print("LISTAS DESDE CERO HASTA VALOR")
+
+valor = int(input("Escriba un número entero: "))
+
+
+if valor > 0:
+  lista = range(0, valor+1)
+  print(list(lista))
+elif valor < 0:
+  lista = range(0, valor-1, -1)
+  print(list(lista))
+else:
+  lista = range(0, valor+1)
+  print(list(lista))
 
